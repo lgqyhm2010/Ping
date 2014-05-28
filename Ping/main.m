dcdecd90eff064fce9853e156a9418c61791845a
lgqyhm2010 <lgqyhm2010@163.com>
2014-05-28 16:11:59 +0800

    Initial commit

---
+++ Ping/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ping
+//
+//  Created by lgqyhm on 14-5-20.
+//  Copyright (c) 2014年 lgqyhm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
